099cfff24e908768c040cf3177f1e90001afdc1a
kashinathpat <kashinathpatkar.2003@gmail.com>
2025-03-22 19:18:29 +0530

    added save/restore app geometry

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import sys
 
 import qdarktheme
+from PyQt6.QtCore import QSettings
 from PyQt6.QtWidgets import QApplication, QStackedWidget
 
 from src.app.HomeGUI import HomeGUI
@@ -10,12 +11,20 @@ import src.ui.res
 class MainApp(QStackedWidget):
     def __init__(self):
         super().__init__()
+        self.settings = QSettings("inventory")
+        self.restore()
         self.login_window = LoginGUI(self)
         self.home_window = HomeGUI(self)
         self.addWidget(self.login_window)
         self.addWidget(self.home_window)
         self.setCurrentIndex(0)
 
+    def restore(self) -> None:
+        self.restoreGeometry(self.settings.value("geometry", b''))
+
+    def closeEvent(self, event) -> None:
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
 
 def main():
     app = QApplication(sys.argv)
