9127ff8107dce4825a0dcf06be22b10704387489
kashinathpat <kashinathpatkar.2003@gmail.com>
2025-04-09 19:09:03 +0530

    added mongodb functions

--- src/utils/database.py
+++ src/utils/database.py
@@ -1,4 +1,43 @@
-def check_login(user_name:str, password: str) -> str:
-    if user_name.strip() == "user123" and password.strip() == "12345678":
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+client = MongoClient("mongodb+srv://root:admin@bookstore.q3yk8d3.mongodb.net/?retryWrites=true&w=majority&appName=BookStore")
+db = client["Store"]
+collection = db["inventory"]
+userCollection = db["users"]
+
+def check_login(username: str, password: str) -> str:
+    user = userCollection.find_one({"username": username.strip(), "password": password.strip()})
+    if user:
         return ""
-    return "Invalid Username or Password.\nPlease check username and password and try Again."
+    return "Invalid Username or Password.\nPlease check username and password and try again."
+
+def getInventoryData():
+  return list(collection.find())
+
+def addItem(name: str, sku: str, price: float, stock: int):
+    item = {
+      "name": name,
+      "sku": sku,
+      "price": price,
+      "stock": stock
+    }
+    result = collection.insert_one(item)
+    return str(result.inserted_id)
+
+
+def updateItem(_id: str, name: str, sku: str, price: float, stock: int):
+  result = collection.update_one(
+    {"_id": ObjectId(_id)},
+    {"$set": {
+      "name": name,
+      "sku": sku,
+      "price": price,
+      "stock": stock
+    }}
+  )
+  return result.modified_count
+
+def deleteItem(_id: str):
+    result = collection.delete_one({"_id": ObjectId(_id)})
+    return result.deleted_count
